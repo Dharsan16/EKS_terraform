4280439f6a934b814168e687a8d3be8de8773608
Dharsan16 <86907338+Dharsan16@users.noreply.github.com>
2026-05-02 10:08:11 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "myapp-vpc"
+  }
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1"
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1"
+}
