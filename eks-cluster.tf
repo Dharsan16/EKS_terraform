c9713ae2df53c20a230b7ac0cd43aa8f6fef42e3
Dharsan16 <86907338+Dharsan16@users.noreply.github.com>
2026-05-02 10:09:56 +0530

    Create eks-cluster.tf

---
+++ eks-cluster.tf
@@ -0,0 +1,11 @@
+resource "aws_eks_cluster" "eks" {
+  name     = var.cluster_name
+  role_arn = aws_iam_role.eks_role.arn
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public_1.id,
+      aws_subnet.public_2.id
+    ]
+  }
+}
