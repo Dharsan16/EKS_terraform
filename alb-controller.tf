7250a51a6a11b7cc0734ec307547bb2701f9617d
Dharsan16 <86907338+Dharsan16@users.noreply.github.com>
2026-05-02 10:30:40 +0530

    Create alb-controller.tf

---
+++ alb-controller.tf
@@ -0,0 +1,12 @@
+resource "helm_release" "alb_controller" {
+  name       = "aws-load-balancer-controller"
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+
+  namespace = "kube-system"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.eks.name
+  }
+}
