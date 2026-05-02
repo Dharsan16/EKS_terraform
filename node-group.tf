089e5d637542e149b59d894a8ac49fa3e581cec5
Dharsan16 <86907338+Dharsan16@users.noreply.github.com>
2026-05-02 10:10:29 +0530

    Create node-group.tf

---
+++ node-group.tf
@@ -0,0 +1,17 @@
+resource "aws_eks_node_group" "nodes" {
+  cluster_name    = aws_eks_cluster.eks.name
+  node_group_name = "myapp-nodes"
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+}
